c167ba4621300fabd08784d212a04d8aeeb4672d
gurudutt shenoy <guruduttgshenoy@gmail.com>
2016-02-06 22:19:30 +1100

    changes the default routing page to
    home/employee
    
    in routeconfig

--- PlacementTracker/App_Start/RouteConfig.cs
+++ PlacementTracker/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace PlacementTracker
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:"home",
+                url:"",
+                defaults: new { controller = "Home", action = "Employee", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Employee", id = UrlParameter.Optional }
             );
         }
     }
